341d2561060ae37c48185dc8e2d5c5b9ceea3d21
Tsvetomir Nikolov <cvetomir.nikolov@gmail.com>
2017-01-02 19:48:46 +0200

    Page title is set dynamically

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import * as GitHub from '../shared/services/github-api';
@@ -13,11 +14,13 @@ export class ProfileComponent implements OnInit {
   user: GitHub.User;
 
   constructor(
+    private title: Title,
     private http: Http,
     private gitHubApi: GitHub.ApiService
   ) { }
 
   ngOnInit() {
+    this.title.setTitle(`${userConfig.githubUsername}`);
     this.gitHubApi.getUser(userConfig.githubUsername)
       .subscribe(user => {
         this.user = user;
